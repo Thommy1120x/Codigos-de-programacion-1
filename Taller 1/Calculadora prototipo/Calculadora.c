7ea9fb3103fa3e6c8acdcd97b6259dfa52437021
Thommy Calderon <thommy.calderon@udla.edu.ec>
2023-04-27 20:54:36 -0500

    Programacion

---
+++ Taller 1/Calculadora prototipo/Calculadora.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include"operacionesbasicas.h"
+int main (){
+int num1, num2, menu;
+    do {    
+        printf ("Leer num 1\n");
+        scanf ("%d",&num1);
+        printf ("Leer num2\n");
+        scanf ("%d",&num2);
+        printf ("Que operacion desea hacer\n 1.Suma\n 2.Resta\n 3.Multiplicacion\n 4.Division\n 5.Salir\n");
+        scanf ("%d",&menu);
+        switch (menu){
+            case 1:
+                Sumar (num1,num2);
+            break;
+
+            case 2:
+                Restar (num1,num2);
+            break;
+
+            case 3:
+                Multiplicar (num1,num2);
+            break;
+
+            case 4:
+                Dividir (num1,num2);
+            break;
+            case 5:
+                printf ("Usted a salido de la calculadora");
+            break;
+        }    
+    }
+    while (menu!=5);
+
+
+return 0;
+}
